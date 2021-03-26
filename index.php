0d1d56354094e82bba58c994529d220c5ff0b2ce
adikkakdela <80500990+adikkakdela@users.noreply.github.com>
2021-03-26 10:00:59 +0600

    Update index.php

--- index.php
+++ index.php
@@ -6,7 +6,7 @@
 </head>
 
 <body>
-<img src="index.jpg" alt="Example" width="250" height="100">
+<img src="index.jpg" alt="Example" width="250" height="250">
 	<h2 style="color:red "> Profile<h2> 
 	<p style="color:orange">Name: Adil <p> 		<p style="color:blue "> Surname: Esengeldiev </p> 
 		<p style="color:green "> Age: 18 </p> 
